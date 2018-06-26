f2ac372969b5663f6e789d3548584949863d5e49
Unknown <tmarhem@elv.telecom-lille.fr>
2018-06-26 11:49:25 +0200

    Added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ContentHomeComponent } from './content-home/content-home.component';
 import { ContentPresentationComponent } from './content-presentation/content-presentation.component';
+
+//Defining routes
+const appRoutes: Routes = [
+  { path: 'home', component: ContentHomeComponent },
+  { path: 'presentation', component: ContentPresentationComponent },
+  { path: 'activities', component: ContentHomeComponent },
+  { path: 'joinUs', component: ContentHomeComponent },
+  { path: 'contact', component: ContentHomeComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   declarations: [
@@ -13,6 +26,12 @@ import { ContentPresentationComponent } from './content-presentation/content-pre
   ],
   imports: [
     BrowserModule
+    //Adding defined routes
+    RouterModule.forRoot(
+      appRoutes,
+      //For debugging
+      { enableTracing: true }  
+      )
   ],
   providers: [],
   bootstrap: [AppComponent]
